9ef98ee32cc0d95de8aebe631b0094fca9b4de0b
Luiz Felipe Machado <88983072+Felpszinho@users.noreply.github.com>
2023-12-11 13:58:51 -0300

    upload exercicio

---
+++ exercicio_ts.js
@@ -0,0 +1,14 @@
+"use strict";
+const numeros = {
+    a: 1,
+    b: 5
+};
+function Resultado(n) {
+}
+const luiz = {
+    nome: "luiz",
+};
+function dizoi(luiz) {
+    return (` ola ${luiz.nome}`);
+}
+console.log(dizoi(luiz));
